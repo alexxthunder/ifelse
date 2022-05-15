27768c64aa962ea61cdc7fb399da39bbc282be23
kitaevalexander <kitaevalexander94@yahoo.com>
2022-05-15 23:24:31 +0700

    factorial

--- src/ifelse.java
+++ src/ifelse.java
@@ -1,3 +1,6 @@
+import java.util.EnumMap;
+import java.util.Enumeration;
+
 public class ifelse {
     public static void main(String[] args) {
 
@@ -6,5 +9,27 @@ public class ifelse {
             System.out.println("Minus deneg");
             cash = cash - 20;
         } while (cash > 0);
+        printMe(45);
+        int s = summ(10,21);
+        printMe(s);
+        System.out.println(factor(10));
+    }
+    public static void printMe(int number) {
+        System.out.println(number);
+    }
+    public static int summ(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+
+    public static int factor(int n) {
+        if (n==0) {
+            return 1;
+        }
+        int result = 1;
+        for (int i = 1; i<=n; i++) {
+            result = result * i;
+        }
+        return result;
     }
 }
