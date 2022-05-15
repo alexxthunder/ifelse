e9601a8acaad5251d28bdf047a19f78794b37b66
kitaevalexander <kitaevalexander94@yahoo.com>
2022-05-15 21:53:30 +0700

    java_2nd lesson

---
+++ src/ifelse.java
@@ -0,0 +1,15 @@
+public class ifelse {
+    public static void main(String[] args) {
+
+        int cash = 10;
+
+        String result;
+
+        if (cash > 60) {
+            result = "Successful payment";
+        } else {
+            result = "No enough money";
+        }
+        System.out.println(result);
+    }
+}
