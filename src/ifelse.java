a2031e82eab2d3d29abdab634a88e6466e7411e7
kitaevalexander <kitaevalexander94@yahoo.com>
2022-05-15 22:10:49 +0700

    many conditions

--- src/ifelse.java
+++ src/ifelse.java
@@ -1,12 +1,15 @@
 public class ifelse {
     public static void main(String[] args) {
 
-        int cash = 100;
+        int cash = 10;
         int age = 18;
 
-        String result;
-
-        result = cash>60&age>=18?"Succesful payment":"Not enough money";
-        System.out.println(result);
+        if (cash > 60) {
+            System.out.println("one condition");
+        } else if (cash < 22) {
+            System.out.printf("Second condition");
+        } else if (cash < 60) {
+            System.out.printf("Third condition");
+        }
     }
 }
