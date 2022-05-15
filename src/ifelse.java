71fc0727ec2d30b19e9236103dc942a940877479
kitaevalexander <kitaevalexander94@yahoo.com>
2022-05-15 22:48:18 +0700

    for each cycle

--- src/ifelse.java
+++ src/ifelse.java
@@ -1,10 +1,10 @@
 public class ifelse {
     public static void main(String[] args) {
 
-        int[] numbers = {3, 5, 10, 22, 12, 42, -8, 21};
-
-        for (int digital: numbers) {
-            System.out.println(digital);
+        int cash = 100;
+        while (cash > 0) {
+            System.out.println("Minus 20 deneg");
+            cash = cash - 20;
         }
     }
 }
